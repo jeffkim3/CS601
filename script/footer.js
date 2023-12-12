d0fb8bd4f55c4259119d2bfb8cbeea36c588f9d2
jeffkim3 <jeffkim@bu.edu>
2023-12-13 05:48:36 +0900

    GitHub Testing
    Testing

--- script/footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const footer = document.getElementById('site-footer');
-    if (footer) {
-        footer.innerHTML = `
-            <p>&copy; ${(new Date()).getFullYear()} Jeffrey Kim. All rights reserved.</p>
-        `;
-    }
-});
--- script/view.js
+++
@@ -1,9 +0,0 @@
-// Function to render resume content
-function renderResumeContent(htmlContent) {
-    const resumeContainer = document.getElementById('resumeContainer');
-    if (resumeContainer) {
-        resumeContainer.innerHTML = htmlContent;
-    }
-}
-
-
